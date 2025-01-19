7f184f1a0d1d0a967eb0a2b1e40bc433c002df6f
Ray <rainer.musch@gmx.de>
2025-01-19 11:56:24 +0100

    Graphic added in canvas

--- script/game.js
+++ script/game.js
@@ -1,3 +1,21 @@
+let canvas;
+let ctx;
+let character = new Image();
+
+// function init() {
+//     canvas = document.getElementById('canvas');
+//     ctx = canvas.getContext('2d');
+//     character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
+//     ctx.drawImage(character, 20, 20, 50, 150);
+// }
+
 function init() {
-    
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
+
+    // Warten, bis das Bild geladen ist
+    character.onload = function() {
+        ctx.drawImage(character, 20, 20, 50, 150);
+    };
 }
