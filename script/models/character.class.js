1ec7374108534aef895a4b0d1997a55141ac89e5
Ray <rainer.musch@gmx.de>
2025-01-20 17:43:43 +0100

    add animation to charachter

--- script/models/character.class.js
+++ script/models/character.class.js
@@ -3,8 +3,31 @@ class Character extends MovableObject {
     height = 280;
     x = 100;
     y = 150;
+    IMAGES_WALKING = [
+        'assets/img/2_character_pepe/2_walk/W-21.png',
+        'assets/img/2_character_pepe/2_walk/W-22.png',
+        'assets/img/2_character_pepe/2_walk/W-23.png',
+        'assets/img/2_character_pepe/2_walk/W-24.png',
+        'assets/img/2_character_pepe/2_walk/W-25.png',
+        'assets/img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
+
     constructor(){
         super().loadImage('assets/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+        let index = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6 
+        // i = 0, 1, 2, 3, 4, 5, nicht 6 sondern 0, 1, 2, ...    
+        let path = this.IMAGES_WALKING[index];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }, 250);    
     }
 
     jump(){
--- script/models/movable-object.class.js
+++ script/models/movable-object.class.js
@@ -4,11 +4,20 @@ class MovableObject {
      img;
      height = 150;
      width = 100; 
+     imageCache = {};
 
 // loadImg('img(test.png')
    loadImage(path){
       this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
       this.img.src = path;
+   }
+
+   loadImages(arr){
+      arr.forEach((path) => {
+         let img = new Image();
+         img.src = path;
+         this.imageCache[path] = img;
+      });   
    }
 
    moveRight() { 
