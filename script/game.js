6608afbe67021a7d89ae03fb673d2127d8d20cb1
Ray <rainer.musch@gmx.de>
2025-01-20 07:05:14 +0100

    try another way to load the img

--- script/game.js
+++ script/game.js
@@ -14,8 +14,13 @@ function init() {
     ctx = canvas.getContext('2d');
     character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
 
+    // // Warten, bis das Bild geladen ist
+    // character.onload = function() {
+    //     ctx.drawImage(character, 20, 20, 50, 150);
+    // };
+
     // Warten, bis das Bild geladen ist
-    character.onload = function() {
+    setTimeout(function () {
         ctx.drawImage(character, 20, 20, 50, 150);
-    };
+    }, 2000);
 }
