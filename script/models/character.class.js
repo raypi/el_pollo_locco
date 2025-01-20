432ad5a72ac77254e948ef812062c6d54fa94acc
Ray <rainer.musch@gmx.de>
2025-01-21 00:06:17 +0100

    add move character to walk in the other direction

--- script/models/character.class.js
+++ script/models/character.class.js
@@ -27,24 +27,18 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
                 this.x += this.speed;
+                this.otherDirection = false;
             }
 
             if (this.world.keyboard.LEFT) {
                 this.x -= this.speed;
+                this.otherDirection = true;
             }
         }, 1000 / 60);
 
         setInterval(() => {
 
-            if(this.world.keyboard.RIGHT){
-                
-                // walk animation
-                let index = this.currentImage % this.IMAGES_WALKING.length;
-                let path = this.IMAGES_WALKING[index];
-                this.img = this.imageCache[path];
-                this.currentImage++;
-            }
-            if(this.world.keyboard.LEFT){
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
                 
                 // walk animation
                 let index = this.currentImage % this.IMAGES_WALKING.length;
