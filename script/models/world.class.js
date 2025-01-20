162b51a5ec59281d0e733888e110decbc9c94b6a
Ray <rainer.musch@gmx.de>
2025-01-21 00:42:08 +0100

    add enlargement of the playing field

--- script/models/world.class.js
+++ script/models/world.class.js
@@ -10,15 +10,32 @@ class World {
         new Cloud()
     ];
     backgroundObject = [
+        new BackgroundObject('assets/img/5_background/layers/air.png', -719),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', -719),
         new BackgroundObject('assets/img/5_background/layers/air.png', 0),
         new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0)
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/air.png', 719),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719),
+        new BackgroundObject('assets/img/5_background/layers/air.png', 1438),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 1438),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 1438),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 1438),
+        new BackgroundObject('assets/img/5_background/layers/air.png', 2157),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 2157),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 2157),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 2157),
+        
     ];
     canvas;
     ctx;
     keyboard;
-    camera_x = -100;
+    camera_x = 0;
 
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
