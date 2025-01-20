91c920d0f7164835cc712b8f7b0881de74b07291
Ray <rainer.musch@gmx.de>
2025-01-21 00:19:24 +0100

    add shifting the background

--- script/models/world.class.js
+++ script/models/world.class.js
@@ -18,6 +18,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = -100;
 
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
@@ -35,10 +36,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObject);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
         this.addToMap(this.character);
+
+        this.ctx.translate(-this.camera_x, 0);
         
         let self = this;
         requestAnimationFrame(function(){
