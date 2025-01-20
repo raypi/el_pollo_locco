9bcf0e279afae503027b35d4b14c8296b4d7584b
Ray <rainer.musch@gmx.de>
2025-01-20 08:00:13 +0100

    add MovableObject

--- script/game.js
+++ script/game.js
@@ -1,6 +1,6 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 // function init() {
 //     canvas = document.getElementById('canvas');
@@ -12,7 +12,6 @@ let character = new Image();
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
 
     // // Warten, bis das Bild geladen ist
     // character.onload = function() {
@@ -20,7 +19,10 @@ function init() {
     // };
 
     // Warten, bis das Bild geladen ist
-    setTimeout(function () {
-        ctx.drawImage(character, 20, 20, 50, 150);
-    }, 2000);
+    // setTimeout(function () {
+    //     ctx.drawImage(character, 20, 20, 50, 150);
+    // }, 2000);
+    
+    console.log('My Character is: ', character);
+    
 }
---
+++ script/models/movable-object.class.js
@@ -0,0 +1,9 @@
+class MovableObject {
+     x = 120;
+     y = 400;
+     img;
+
+     moveRight(params) { // das wort function kann auch entfertt werden da es nicht benötigt wird(drf nicht)
+        console.log('Moving right!');
+     }
+}
