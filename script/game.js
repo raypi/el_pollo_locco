ec2e9e5a039736a2161ce78a277b221984820734
Ray <rainer.musch@gmx.de>
2025-01-21 10:52:15 +0100

    add fixed move

--- script/game.js
+++ script/game.js
@@ -8,7 +8,7 @@ function init() {
     world = new World(canvas, keyboard);
     
     console.log('My Character is: ', world.character);
-    console.log('The enemies is: ', world.enemies);    
+    console.log('The enemies is: ', world.enemies);    // aktuell nicht mehr verknüpft
 }
 
 
