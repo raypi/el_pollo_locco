3f85af4894e22eead4ce1f791c99457fd295e180
Ray <rainer.musch@gmx.de>
2025-01-22 07:11:38 +0100

    Functions divided to make the code more readable

--- script/models/movable-object.class.js
+++ script/models/movable-object.class.js
@@ -32,6 +32,18 @@ class MovableObject {
       this.img.src = path;
    }
 
+   draw(ctx){
+      ctx.drawImage(this.img, this.x, this.y,  this.width, this.height);
+   }
+
+   drawBox(ctx) {
+      ctx.beginPath();
+      ctx.lineWidth = '5';
+      ctx.strokeStyle = 'blue';
+      ctx.rect(this.x, this.y,  this.width, this.height);
+      ctx.stroke();
+   }
+
    loadImages(arr){
       arr.forEach((path) => {
          let img = new Image();
