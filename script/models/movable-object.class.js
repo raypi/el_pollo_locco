bc1d60493dc4809a18da59bd81cc08c01b47a8ef
Ray <rainer.musch@gmx.de>
2025-01-28 12:27:38 +0100

    add img for coin bar

--- script/models/movable-object.class.js
+++ script/models/movable-object.class.js
@@ -1,4 +1,4 @@
-class MovableObject extends DrawableObject {
+class MovableObject extends DrawableObject { // bewegbare Objekte
      currentImage = 0;
      speed = 0.15;
      otherDirection = false;
--- script/models/status-bar.class.js
+++ script/models/status-bar.class.js
@@ -9,6 +9,28 @@ class StatusBar extends DrawableObject {
         'assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
         'assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
     ];
+
+    IMAGES_COIN [
+        'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
+        'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
+        'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
+        'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
+        'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
+        'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png'
+    ];
+
+    IMAGES_BOTTLE [
+        'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
+        'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
+        'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
+        'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
+        'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
+        'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
+    ];
+
+
+
+
     percentage = 100;
 
     constructor(){
