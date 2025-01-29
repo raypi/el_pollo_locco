350641afe80b1ddf6d414f8f77a4e2e64d0b03af
Ray <rainer.musch@gmx.de>
2025-01-29 17:56:22 +0100

    add constructor and animate to SmalChicken class

---
+++ script/models/smalchicken.class.js
@@ -0,0 +1,33 @@
+class SmalChicken extends MovableObject {
+    width = 60;
+    height = 60;
+    y = 365;
+    IMAGES_WALKING = [
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+    
+
+    constructor(){
+        super().loadImage('assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 500 + Math.random() * 500;
+        this.speed = 0.15 + Math.random() + 0.25;
+        this.animate();
+    }
+   
+
+    animate(){
+        this.moveLeft();
+
+        setInterval(() => {
+            let index = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6 
+            // i = 0, 1, 2, 3, 4, 5, nicht 6 sondern 0, 1, 2, ...    
+            let path = this.IMAGES_WALKING[index];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);    
+    }
+}
