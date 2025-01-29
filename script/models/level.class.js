4a8a7df6311bf81a897a625e053a2ee523390f97
Ray <rainer.musch@gmx.de>
2025-01-29 18:17:55 +0100

    add smalChicken Arry and init in Level class

--- script/models/level.class.js
+++ script/models/level.class.js
@@ -5,16 +5,18 @@ class Level {
     backgroundObject;
     coins;
     bottles;
+    smalChicken;
     level_end_x = 2255;
 
 
 
-constructor(enemies, clouds, backgroundObject, endboss, coins, bottles) {
+constructor(enemies, clouds, backgroundObject, endboss, coins, bottles, smalChicken) {
     this.enemies = enemies;
     this.clouds = clouds;
     this.endboss = endboss;
     this.backgroundObject = backgroundObject;
     this.coins = coins;
     this.bottles = bottles;
+    this.smalChicken = smalChicken;
 }
 }
