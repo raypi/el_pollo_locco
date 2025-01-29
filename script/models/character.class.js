df6b7117e9c6e9b8b0ae2b2ea8b3bd873f781b5c
Ray <rainer.musch@gmx.de>
2025-01-29 19:04:14 +0100

    add idle img to character

--- script/models/character.class.js
+++ script/models/character.class.js
@@ -40,6 +40,23 @@ class Character extends MovableObject {
         'assets/img/2_character_pepe/4_hurt/H-42.png',
         'assets/img/2_character_pepe/4_hurt/H-43.png'
     ];
+
+    IMAGES_IDLE = [
+        'assets/img/2_character_pepe/1_idle/idle/I-1.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-2.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-3.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-4.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-5.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-6.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-7.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-8.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-9.png',
+        'assets/img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
+
+
+
+
     // currentImage = 0;
     world;
 
