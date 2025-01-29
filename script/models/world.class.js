074ffd19830c0c019d5208eb6553f44c98ad7dc0
Ray <rainer.musch@gmx.de>
2025-01-29 18:22:32 +0100

    add smalChicken to draw in world

--- script/models/world.class.js
+++ script/models/world.class.js
@@ -80,6 +80,7 @@ class World {
         this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.level.smalChicken);
     
         
         
