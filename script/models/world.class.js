fa653d4ece69a9ddb2fe0215704f0a05fd27793f
Ray <rainer.musch@gmx.de>
2025-01-29 14:56:59 +0100

    add debug to World

--- script/models/world.class.js
+++ script/models/world.class.js
@@ -23,6 +23,9 @@ class World {
         this.draw();
         this.setWorld();
         this.run();
+        // D E B U G
+        console.log('DG Constructor World, Coins:', this.level.coins);
+        console.log('DG Constructor World, Bottles:', this.level.bottles);
     }
 
     setWorld() {
@@ -76,14 +79,13 @@ class World {
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
     
+        
+        
         // Coins und Bottles zeichnen
-        
-        
         this.addObjectsToMap(this.level.bottles);
-        // console.log('Bottel:', this.level.bottles);
-        
+        console.log('DG World Draw, Bottel:', this.level.bottles);
         this.addObjectsToMap(this.level.coins);
-        // console.log('Coins:', this.level.coins);
+        console.log('DG World Draw, Coins:', this.level.coins);
     
         this.addObjectsToMap(this.throwableObjects);
         this.addToMap(this.character);
