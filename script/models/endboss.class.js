4cb47241ed91c9617c7bde3807ecfa7b97dd18c4
Ray <rainer.musch@gmx.de>
2025-02-04 18:06:08 +0100

    add animation to endboss

--- script/models/endboss.class.js
+++ script/models/endboss.class.js
@@ -7,7 +7,7 @@ class Endboss extends MovableObject{
         'assets/img/4_enemie_boss_chicken/1_walk/G1.png',
         'assets/img/4_enemie_boss_chicken/1_walk/G2.png',
         'assets/img/4_enemie_boss_chicken/1_walk/G3.png',
-        'assets/img/4_enemie_boss_chicken/1_walk/G4.png'
+        'assets/img/4_enemie_boss_chicken/1_walk/G4.png' 
     ];
   
     IMAGES_ALERT = [
