162e63deffd3247b4a2b4ebd4bd7d90d57d14029
Ray <rainer.musch@gmx.de>
2025-02-05 16:04:44 +0100

    add smal chichen img dead

--- script/models/smalchicken.class.js
+++ script/models/smalchicken.class.js
@@ -7,11 +7,16 @@ class SmalChicken extends MovableObject {
         'assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
         'assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
+
+    IMAGES_DEAD = [
+        'assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
     
 
     constructor(){
         super().loadImage('assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
 
         this.x = 500 + Math.random() * 500;
         this.speed = 0.15 + Math.random() + 0.25;
