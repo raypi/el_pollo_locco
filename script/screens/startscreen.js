aa136eaa21adb7e879f32aada9a5dcfd7965c12b
Ray <rainer.musch@gmx.de>
2025-02-06 11:11:28 +0100

    add draw to StartScreen

--- script/screens/startscreen.js
+++ script/screens/startscreen.js
@@ -15,4 +15,25 @@ class StartScreen {
         ];
     }
 
+    draw() {
+        // Hintergrund zeichnen
+        this.ctx.drawImage(this.backgroundImage, 0, 0, this.canvas.width, this.canvas.height);
+
+        // Buttons zeichnen
+        this.ctx.font = '30px Arial';
+        this.ctx.textAlign = 'center';
+        this.ctx.fillStyle = 'white';
+        this.buttons.forEach((button, index) => {
+            const x = this.canvas.width / 2;
+            const y = 200 + index * 60; // Vertikaler Abstand zwischen den Buttons
+            this.ctx.fillText(button.label, x, y);
+            button.x = x - 100; // Für Klickerkennung
+            button.y = y - 30;
+            button.width = 200;
+            button.height = 40;
+        });
+    }
+
+
+
 }
