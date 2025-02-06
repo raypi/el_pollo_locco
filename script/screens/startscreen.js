0c4990ac36188ea2042d2e91a8d05a1631576b09
Ray <rainer.musch@gmx.de>
2025-02-06 10:22:05 +0100

    update constructor

--- script/screens/startscreen.js
+++ script/screens/startscreen.js
@@ -1,3 +1,18 @@
 class StartScreen {
     
+    constructor(game) {
+        this.game = game; 
+        this.canvas = document.getElementById('gameCanvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.backgroundImage = new Image();
+        this.backgroundImage.src = 'assets/img/9_intro_outro_screens/start/startscreen_1.png';
+        this.buttons = [
+            { label: 'Start', action: () => this.startGame() },
+            { label: 'Steuerung', action: () => this.showControls() },
+            { label: 'Erklärung', action: () => this.showExplanation() },
+            { label: 'Erklärung', action: () => this.showExplanation() },
+            { label: 'Sound', action: () => this.toggleSound() },
+        ];
+    }
+
 }
