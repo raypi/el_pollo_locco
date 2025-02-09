27c5fec8b9decc48409b6ba58764d8cd81505c9d
Ray <rainer.musch@gmx.de>
2025-02-09 08:12:57 +0100

    update Key Down and Up

--- script/game.js
+++ script/game.js
@@ -55,39 +55,30 @@ function startGame() {
 
 
 
-window.addEventListener('keydown', (event) => {
-    // console.log(event.keyCode);
-    if(event.keyCode == 39){
-        keyboard.RIGHT = true;
-    }
+// Tastatur-Eventhandler
+function keyboardKeyDown(event) {
+    if (event.keyCode == 39) keyboard.RIGHT = true;
+    if (event.keyCode == 37) keyboard.LEFT = true;
+    if (event.keyCode == 38) keyboard.UP = true;
+    if (event.keyCode == 40) keyboard.DOWN = true;
+    if (event.keyCode == 32) keyboard.SPACE = true;
+    if (event.keyCode == 77) keyboard.M = true;
+    if (event.keyCode == 78) keyboard.N = true;
+    // ggf Else Taste nicht belegt ton
+}
 
-    if(event.keyCode == 37){
-        keyboard.LEFT = true;
-    }
+function keyboardKeyUp(event) {
+    if (event.keyCode == 39) keyboard.RIGHT = false;
+    if (event.keyCode == 37) keyboard.LEFT = false;
+    if (event.keyCode == 38) keyboard.UP = false;
+    if (event.keyCode == 40) keyboard.DOWN = false;
+    if (event.keyCode == 32) keyboard.SPACE = false;
+    if (event.keyCode == 77) keyboard.M = false;
+    if (event.keyCode == 78) keyboard.N = false;
+}
 
-    if(event.keyCode == 38){
-        keyboard.UP = true;
-    }
-
-    if(event.keyCode == 40){
-        keyboard.DOWN = true;
-    }
-
-    if(event.keyCode == 32){
-        keyboard.SPACE = true;
-    }
-
-    if(event.keyCode == 77){
-        keyboard.M = true;
-    }
-
-    if(event.keyCode == 78){
-        keyboard.N = true;
-    }
-
-    // Else für Warnton wegen falscher Taste, ggf. ausgabe in DIV
-});
-
+// Sobald das DOM geladen ist, wird init() aufgerufen.
+window.addEventListener('load', init);
 
 window.addEventListener('keyup', (event) => {
 
