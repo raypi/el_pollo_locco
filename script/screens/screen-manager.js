c161427b58513bde1aab48151c0608749903e7a2
Ray <rainer.musch@gmx.de>
2025-02-09 08:15:41 +0100

    update constructor at Screen Manager

--- script/screens/screen-manager.js
+++ script/screens/screen-manager.js
@@ -1,6 +1,10 @@
 class ScreenManager {
 
     constructor(){
-        
+    
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        // Aktueller Screen (z. B. StartScreen, GameOverScreen)
+        this.currentScreen = null;
     }
 }
