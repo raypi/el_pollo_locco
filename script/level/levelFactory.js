fa845afa2945454b0aae7b42a0d9186ae2e98bd2
Ray <rainer.musch@gmx.de>
2025-02-15 14:29:04 +0100

    add items to createLevel001

--- script/level/levelFactory.js
+++ script/level/levelFactory.js
@@ -1,3 +1,57 @@
 function createLevel001() {
-
+    return new Level(
+        [
+          // Gegner, z. B.:
+          // new Chicken(),
+          // ...
+        ],
+        [
+          new Cloud()
+        ],
+        [
+          new BackgroundObject('assets/img/5_background/layers/air.png', -719),
+          new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', -719),
+          new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', -719),
+          new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', -719),
+          new BackgroundObject('assets/img/5_background/layers/air.png', 0),
+          new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0),
+          new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0),
+          new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0),
+          new BackgroundObject('assets/img/5_background/layers/air.png', 719),
+          new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719),
+          new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719),
+          new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719),
+          new BackgroundObject('assets/img/5_background/layers/air.png', 1438),
+          new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 1438),
+          new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 1438),
+          new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 1438),
+          new BackgroundObject('assets/img/5_background/layers/air.png', 2157),
+          new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 2157),
+          new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 2157),
+          new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 2157)
+        ],
+        [
+          // Erstelle den Endboss neu und übergebe den Callback:
+          new Endboss((resultType) => {
+              screenManager.showGameOverScreen(resultType);
+          }),
+        ],
+        [
+          new Coins(1200, 320),
+          new Coins(1300, 250),
+          new Coins(1400, 200),
+          new Coins(1500, 250),
+          new Coins(1600, 320),
+        ],
+        [
+          new Bottles(400, 280),
+          new Bottles(500, 280),
+          new Bottles(600, 280),
+          new Bottles(700, 280),
+          new Bottles(800, 280),
+        ],
+        [
+          // Optional: kleine Gegner, z.B. new SmalChicken(), ...
+        ]
+      );
   }
