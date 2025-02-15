cd9c8e6d66ceabe1eb21d0e0f61c93b160c76053
Ray <rainer.musch@gmx.de>
2025-02-15 10:39:30 +0100

    add basic structure of the script

--- script/models/sound/audioHub.class.js
+++ script/models/sound/audioHub.class.js
@@ -1,3 +1,15 @@
 class AudioHub {
    // grundsätzliche steuerung aller sounds im spiel
+
+   // Audiodateien laden
+
+   // Array für alle audio Dateien
+   
+    // methode zum abspielen einzelner Sounds
+
+    // methode zum stoppen aller Sounds
+
+    // methode zum stoppen eines sounds
+
+    // Anmerkung. ggf. kann man eine methode einfügen die die Lautstärke aller Sounds regelt
 }
