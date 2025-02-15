b3044f0aade5b3a42d0f660b49a5a40524bc98dd
Ray <rainer.musch@gmx.de>
2025-02-15 11:10:59 +0100

    add sounds to AudioHub

--- script/models/sound/audioHub.class.js
+++ script/models/sound/audioHub.class.js
@@ -2,6 +2,16 @@ class AudioHub {
    // grundsätzliche steuerung aller sounds im spiel
 
    // Audiodateien laden
+   static CHICKENHIT = new Audio('assets/audio/ChickenSound.mp3');
+   static COINBARCOLLECT = new Audio('assets/audio/coinCollect.wav');
+   static COINBARCOUNT = new Audio('assets/audio/coinCount.wav');
+   static DEATH = new Audio('assets/audio/death.wav');
+   static ENDBOSSBATTLE = new Audio('assets/audio/enbossBattle.ogg');
+   static JUMP = new Audio('assets/audio/jump.wav');
+   static GAMEMUSIC = new Audio('assets/audio/gamemusic.wav');
+
+
+
 
    // Array für alle audio Dateien
    
