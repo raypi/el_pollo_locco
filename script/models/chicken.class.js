3dfc12c4410ffb76421b97a19a43dcf249644cd1
Ray <rainer.musch@gmx.de>
2025-04-09 18:12:06 +0200

    update chicken class with alive

--- script/models/chicken.class.js
+++ script/models/chicken.class.js
@@ -2,6 +2,8 @@ class Chicken extends MovableObject {
     width = 60;
     height = 60;
     y = 365;
+    alive = true;
+    
     IMAGES_WALKING = [
         'assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
