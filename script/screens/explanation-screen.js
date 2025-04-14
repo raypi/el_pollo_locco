832a2e8ef1414b9a28948f56a45a22561bdc9713
Ray <rainer.musch@gmx.de>
2025-04-14 09:40:37 +0200

    update eventlistener exlpanation screen for touche device

--- script/screens/explanation-screen.js
+++ script/screens/explanation-screen.js
@@ -169,21 +169,21 @@ class ExplanationScreen {
     });
   }
 
-  /**
-   * Event-Listener hinzufügen (mousemove + click)
-   */
-  addEventListeners() {
-    this.canvas.addEventListener('mousemove', this.boundHandleMouseMove);
+   // Methode zum Hinzufügen der Event Listener (jetzt mit Touch-Events)
+   addEventListeners() {
     this.canvas.addEventListener('click', this.boundHandleClick);
-  }
+    this.canvas.addEventListener('mousemove', this.boundHandleMouseMove);
+    this.canvas.addEventListener('touchstart', this.boundHandleTouchStart);
+    this.canvas.addEventListener('touchmove', this.boundHandleTouchMove);
+}
 
-  /**
-   * Event-Listener entfernen
-   */
-  removeEventListeners() {
-    this.canvas.removeEventListener('mousemove', this.boundHandleMouseMove);
+// Methode zum Entfernen der Event Listener (sowohl Mouse als auch Touch)
+removeEventListeners() {
     this.canvas.removeEventListener('click', this.boundHandleClick);
-  }
+    this.canvas.removeEventListener('mousemove', this.boundHandleMouseMove);
+    this.canvas.removeEventListener('touchstart', this.boundHandleTouchStart);
+    this.canvas.removeEventListener('touchmove', this.boundHandleTouchMove);
+}
 }
 
 
